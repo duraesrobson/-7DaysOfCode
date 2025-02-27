408974fad16f114fbf6d993a5521c569185a5d47
Robson Durães <duraesrobson@gmail.com>
2025-02-27 02:35:11 -0300

    alteracoes no css e js

--- script.js
+++ script.js
@@ -5,25 +5,36 @@ let linguagem;
 let gosta;
 
 
-document.getElementById("btnEnviar").onclick = function() {
+document.getElementById("btnEnviar").onclick = function () {
     nome = document.getElementById("nome").value;
     idade = document.getElementById("idade").value;
     linguagem = document.getElementById("linguagem").value;
-    document.getElementById("desafio__dia2__resposta").textContent = `Olá, ${nome}! Você tem ${idade} anos e já está aprendendo ${linguagem}!`;
 
-    document.getElementById("linguagem__span").textContent = linguagem
-    document.getElementById("segunda__pergunta").style.display = "block";
-
+    if (nome && idade && linguagem) {
+        document.getElementById("desafio__dia2__resposta").textContent = `Olá, ${nome}! Você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+        document.getElementById("linguagem__span").textContent = linguagem
+        document.getElementById("segunda__pergunta").style.display = "block";
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
 }
 
-document.getElementById("btnGosta").onclick = function() {
+document.getElementById("btnGosta").onclick = function () {
     gosta = document.getElementById("gosta").value;
 
-    if (gosta == 1) {
+    if (gosta !== "1" && gosta !== "2") {
+        alert("Digite 1 ou 2!");
+    } else if (gosta == 1) {
         document.getElementById("segunda__pergunta__resposta").textContent = "Muito bom! Continue estudando e você terá muito sucesso.";
     } else {
-        document.getElementById("segunda__pergunta__resposta").textContent = "Ahh que pena... Já tentou aprender outras linguagens?"
-        }
+        document.getElementById("segunda__pergunta__resposta").textContent = "Ahh que pena... Já tentou aprender outras linguagens?";
+    }
+
+    document.getElementById("btnReset").style.display = "block";
+}
+
+document.getElementById("btnReset").onclick = function () {
+    location.reload();
 }
 
 
